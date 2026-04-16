ea10faac3dbec76cbc18152ef3c4b91ade66a508
levikaua07-spec <levikaua07@gmail.com>
2026-04-15 23:37:40 -0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = "https://sovofgvgwemjvnywjmuh.supabase.co";
+const SUPABASE_KEY = "sb_publishable_vY5lPtnzP9saVdtM85QhNQ_Q-8WHWdO";
+
+// Garante que o cliente seja criado corretamente
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
